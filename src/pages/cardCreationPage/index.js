ee3c15f943127ffc5fc786bca4ebec58c9be7455
HyuuunjuKim <43339385+HyuuunjuKim@users.noreply.github.com>
2021-05-02 12:48:09 +0900

    refactor: targetCardId를 전역 상태로 변경

--- src/pages/cardCreationPage/index.js
+++ src/pages/cardCreationPage/index.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { firestore } from '../../firebase';
 import { Button } from '../../components/commons/button/Button';
 import { Header } from '../../components/commons/header/Header';
@@ -13,42 +13,22 @@ import CardPasswordInput from '../../components/cardCreation/cardPasswordInput/C
 import Styled from './style';
 import { COLOR } from '../../constants/color';
 import { PAGE } from '../../constants/page';
+import { CARD_NUMBER_INPUT, CARD_PASSWORD_INPUT, EXPIRED_DATE_INPUT } from '../../constants/input';
+import { TargetCardIdContext } from '../../contexts/TargetCardIdContext';
 import {
-  CARD_NUMBER_INPUT,
-  CARD_OWNER_INPUT,
-  CARD_PASSWORD_INPUT,
-  EXPIRED_DATE_INPUT,
-  SECURITY_CODE_INPUT,
-} from '../../constants/input';
-import { isFilledAllNumber } from '../../utils';
-
-const isValidCardNumberInput = cardNumber => {
-  return Object.values(cardNumber).every(cardNumber => isFilledAllNumber(cardNumber, CARD_NUMBER_INPUT.LENGTH));
-};
-const isValidMonthInput = cardExpiredDate => {
-  const month = Number(cardExpiredDate.month);
-
-  return (
-    EXPIRED_DATE_INPUT.RANGE.MONTH.MIN <= month &&
-    month <= EXPIRED_DATE_INPUT.RANGE.MONTH.MAX &&
-    isFilledAllNumber(cardExpiredDate.month, EXPIRED_DATE_INPUT.LENGTH)
-  );
-};
-const isValidYearInput = cardExpiredDate => {
-  const year = Number(cardExpiredDate.year);
-
-  return (
-    EXPIRED_DATE_INPUT.RANGE.YEAR.MIN <= year && isFilledAllNumber(cardExpiredDate.year, EXPIRED_DATE_INPUT.LENGTH)
-  );
-};
-const isValidCardOwnerInput = cardOwner => cardOwner.length <= CARD_OWNER_INPUT.LENGTH.MAX;
-const isValidSecurityCodeInput = securityCode => isFilledAllNumber(securityCode, SECURITY_CODE_INPUT.LENGTH);
-const isValidCardPasswordInput = cardPassword =>
-  Object.values(cardPassword).every(cardPassword => isFilledAllNumber(cardPassword, CARD_PASSWORD_INPUT.LENGTH));
+  isValidCardNumberInput,
+  isValidCardOwnerInput,
+  isValidCardPasswordInput,
+  isValidMonthInput,
+  isValidSecurityCodeInput,
+  isValidYearInput,
+} from './validates';
 
 const cardListRef = firestore.collection('cardList');
 
-const CardCreationPage = ({ targetCardId, setCurrentPage, setNewCardInfo, setTargetCardId }) => {
+const CardCreationPage = ({ setCurrentPage, setNewCardInfo }) => {
+  const { state, actions } = useContext(TargetCardIdContext);
+
   const [cardOwner, setCardOwner] = useState('');
   const [securityCode, setSecurityCode] = useState('');
   const [cardNumber, setCardNumber] = useState({
@@ -69,7 +49,7 @@ const CardCreationPage = ({ targetCardId, setCurrentPage, setNewCardInfo, setTar
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await cardListRef.doc(targetCardId).get();
+      const response = await cardListRef.doc(state.targetCardId).get();
       const { cardNumber, cardExpiredDate, cardOwner, securityCode, cardPassword, selectedCardInfo } = response.data();
 
       setCardNumber(cardNumber);
@@ -80,7 +60,7 @@ const CardCreationPage = ({ targetCardId, setCurrentPage, setNewCardInfo, setTar
       setSelectedCardInfo(selectedCardInfo);
     };
 
-    if (targetCardId) {
+    if (state.targetCardId) {
       fetchData();
     }
   }, []);
@@ -103,7 +83,7 @@ const CardCreationPage = ({ targetCardId, setCurrentPage, setNewCardInfo, setTar
   };
 
   const handlePrevIconClick = () => {
-    setTargetCardId('');
+    actions.setTargetCardId('');
     setCurrentPage(PAGE.CARD_LIST);
   };
 
